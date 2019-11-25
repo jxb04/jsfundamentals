0a7058ad616018410bf34ad1ea4f2376de57a0dd
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-25 15:50:56 -0500

    language features

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+let car = { id: 5000, style: 'convertible' };
+let {id, style} = car;
+
+console.log(id, style);
+
+function startCars(car1, car2, car3) {
+    console.log(car1, car2, car3);
+};
+
+let carIds = [1, 2, 3];
+startCars(...carIds);
