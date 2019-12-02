759a219a779f43adc5c2f62535bfccdb2893ded3
Joel Bernasky <joel_bernasky@hotmail.com>
2019-12-01 20:34:11 -0500

    constructor function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -49,3 +49,13 @@ let appClosure = (function() {
     };
 })();
 console.log(appClosure.getId());
+
+function Car (id) {
+    this.carId = id;
+    this.start = function () {
+        console.log('start: ' + this.carId);
+    };
+};
+
+let vehicle = new Car(123);
+vehicle.start();
