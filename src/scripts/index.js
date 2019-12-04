b1791ae813d8d92665ff6c3d0f9073efa5579327
Joel Bernasky <joel_bernasky@hotmail.com>
2019-12-04 12:33:21 -0500

    modify DOM elements

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,8 @@ console.log(el);
 let els = document.getElementsByClassName('p1');
 
 console.log (els);
+
+let element = document.getElementById('first');
+
+element.textContent = 'New content';
+element.style.color = 'blue';
