c31ed15ad11c91e257c34b92d5bbf15243d22a54
Joel Bernasky <joel_bernasky@hotmail.com>
2019-12-04 16:16:54 -0500

    forms

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,17 @@
-let promise = new Promise(
-    function(resolve, reject) {
-        setTimeout(resolve, 100, 'someValue');
-    }
-);
-promise.then (
-    value => console.log('fulfilled: ' + value),
-    error => console.log('rejected: ' + error)
-);
+let form = document.getElementById('user-form');
+
+form.addEventListener('submit', event=> {
+
+    let user = form.elements['user'];
+    let userError = document.getElementById('user-error');
+    let avatarFile = form.element['avatar-file'];
+
+    userError.textContent = 'Invalid entry';
+    userError.style.color = 'red';
+    user.style.borderColor = 'red';
+    user.focus();
+
+    console.log (user.value, avatarFile.value);
+    //prevent the browser from submitting the form
+    event.preventDefault();
+});
