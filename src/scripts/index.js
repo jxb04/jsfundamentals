619c3b837954c08f1797e98202f5cfe53249bf22
Joel Bernasky <joel_bernasky@hotmail.com>
2019-12-05 08:50:11 -0500

    chrome tools

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,1 @@
-let form = document.getElementById('user-form');
-
-form.addEventListener('submit', event=> {
-
-    let user = form.elements['user'];
-    let userError = document.getElementById('user-error');
-    let avatarFile = form.element['avatar-file'];
-
-    userError.textContent = 'Invalid entry';
-    userError.style.color = 'red';
-    user.style.borderColor = 'red';
-    user.focus();
-
-    console.log (user.value, avatarFile.value);
-    //prevent the browser from submitting the form
-    event.preventDefault();
-});
+window.foo = 'secret code';
